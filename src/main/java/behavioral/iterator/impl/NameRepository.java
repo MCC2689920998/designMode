ebf9eed65121646d41ad491fa8478351936b12dd
machangchang <2689920998@qq.com>
2019-07-31 10:47:42 +0800

    feat: 迭代器模式，案例提交；

---
+++ src/main/java/behavioral/iterator/impl/NameRepository.java
@@ -0,0 +1,38 @@
+package behavioral.iterator.impl;
+
+import behavioral.iterator.Container;
+import behavioral.iterator.Iterator;
+
+/**
+ * @Author MCC
+ * @Create 2019/7/31 10:43
+ */
+public class NameRepository implements Container {
+    public String names[] = {"Robert", "John", "Julie", "Lora"};
+
+    @Override
+    public Iterator getIterator() {
+        return new NameIterator();
+    }
+
+    private class NameIterator implements Iterator {
+
+        int index;
+
+        @Override
+        public boolean hasNext() {
+            if (index < names.length) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            if (this.hasNext()) {
+                return names[index++];
+            }
+            return null;
+        }
+    }
+}
---
+++ src/test/java/behavioral/iterator/ContainerTest.java
@@ -0,0 +1,19 @@
+package behavioral.iterator;
+
+import behavioral.iterator.impl.NameRepository;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ContainerTest {
+    @Test
+    public void getIterator() throws Exception {
+        NameRepository namesRepository = new NameRepository();
+
+        for(Iterator iter = namesRepository.getIterator(); iter.hasNext();){
+            String name = (String)iter.next();
+            System.out.println("Name : " + name);
+        }
+    }
+
+}
